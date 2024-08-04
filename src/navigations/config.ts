2f2f71f784c5b9690f1a8f44beedba5d02875df0
vanh1604 <VietAnhScout@gmail.com>
2024-08-04 13:26:12 +0700

    finish

--- src/navigations/config.ts
+++ src/navigations/config.ts
@@ -1,9 +1,15 @@
+import { EData } from "../db/home-data";
+import { Exercise } from "../db/practice";
+
 export type RootStackParams = {
 	TabNavigation: undefined;
 	QuizzResult: undefined;
 	QuizzScreen: undefined;
-	PracticeDetail: undefined;
+	PracticeDetail: {
+		index: number;
+	} & Exercise;
 	Detail: undefined;
+	DetailCommon: {id:number,dataType: EData};
 	PuzzleScreen: { id: number };
 };
 
